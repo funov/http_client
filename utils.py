d4301c916c5d31ee196f5cd90a28e8a319e55e4f
Ivan Sychev <funovichf@gmail.com>
2022-05-08 14:33:39 +0500

    refactoring

--- utils.py
+++ utils.py
@@ -110,7 +110,9 @@ def write_http_response(url, http_method, http_version, headers,
         if "Content-Length" not in response.headers.keys():
             return None
 
-        img_response = get_img_response(response.bytes_response, response.headers)
+        img_response = get_img_response(
+            response.bytes_response,
+            response.headers)
         write_image(f'{time}/{image_name}', img_response)
     elif response.decoded_response is not None:
         write_file(os.path.join(time, "result.txt"),
